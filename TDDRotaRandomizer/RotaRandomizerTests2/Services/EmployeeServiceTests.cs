8a6a391338a23701b0af1c7c07de8da252e643f5
Patricia <psdr1994@hotmail.com>
2020-11-01 18:31:20 +0000

    Testing Services - Start
    Employee Service Test

--- TDDRotaRandomizer/RotaRandomizerTests2/Services/EmployeeServiceTests.cs
+++ TDDRotaRandomizer/RotaRandomizerTests2/Services/EmployeeServiceTests.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using RotaRandomizer.Domain.Repositories;
 using RotaRandomizer.Domain.Services;
 using RotaRandomizer.Models;
+using RotaRandomizer.Persistence.Contexts;
+using RotaRandomizer.Persistence.Repositories;
 using RotaRandomizer.Services;
 using System;
 using System.Collections.Generic;
@@ -14,37 +18,82 @@ namespace RotaRandomizer.Services.Tests
     [TestClass()]
     public class EmployeeServiceTests
     {
-        private Mock<IEmployeeService> _employeeService;
+        private RotaDbContext _context;
+        private IEmployeeRepository _employeeRepository;
+        private IEmployeeService _employeeService;
 
-        public EmployeeServiceTests()
-        {            
-            _employeeService = new Mock<IEmployeeService>();
+        [ClassInitialize]
+        public void ClassSetUp()
+        {
+            var options = new DbContextOptionsBuilder<RotaDbContext>()
+               .UseInMemoryDatabase("RotaRandomizer")
+               .Options;
+            _context = new RotaDbContext(options);
+            _context.Employees.Add(new Employee { Id = 1, Name = "John", EmployeeNumber = "E123" });
+            _context.Employees.Add(new Employee { Id = 2, Name = "Jane", EmployeeNumber = "E234" });
+            _context.Employees.Add(new Employee { Id = 3, Name = "Jamie", EmployeeNumber = "E345" });
+            _context.Employees.Add(new Employee { Id = 4, Name = "Janette", EmployeeNumber = "E456" });
+            _context.Employees.Add(new Employee { Id = 5, Name = "Joshua", EmployeeNumber = "E567" });
+            _context.Employees.Add(new Employee { Id = 6, Name = "Jack", EmployeeNumber = "E678" });
+            _context.Employees.Add(new Employee { Id = 7, Name = "Jeremy", EmployeeNumber = "E789" });
+            _context.Employees.Add(new Employee { Id = 8, Name = "Jennifer", EmployeeNumber = "E890" });
+            _context.Employees.Add(new Employee { Id = 9, Name = "Jasper", EmployeeNumber = "E901" });
+            _context.Employees.Add(new Employee { Id = 10, Name = "Joselyn", EmployeeNumber = "E012" });
+            _context.SaveChanges();
+            _employeeRepository = new EmployeeRepository(_context);
+            _employeeService = new EmployeeService(_employeeRepository);
         }
+
+
+        [ClassInitialize]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+
 
         [TestMethod()]
         public async Task ListAsyncTest()
         {
-            IEnumerable<Employee> employees = new List<Employee>  {
-               new Employee { Id = 1, Name = "John", EmployeeNumber = "E123" },
-               new Employee { Id = 2, Name = "Jane", EmployeeNumber = "E234" },
-               new Employee { Id = 3, Name = "Jamie", EmployeeNumber = "E345" },
-               new Employee { Id = 4, Name = "Janette", EmployeeNumber = "E456" },
-               new Employee { Id = 5, Name = "Joshua", EmployeeNumber = "E567" },
-               new Employee { Id = 6, Name = "Jack", EmployeeNumber = "E678" },
-               new Employee { Id = 7, Name = "Jeremy", EmployeeNumber = "E789" },
-               new Employee { Id = 8, Name = "Jennifer", EmployeeNumber = "E890" },
-               new Employee { Id = 9, Name = "Jasper", EmployeeNumber = "E901" },
-               new Employee { Id = 10, Name = "Joselyn", EmployeeNumber = "E012" }
-                };
-            _employeeService.Setup(mr => mr.ListAsync()).Returns(Task.FromResult(employees));
-            List<Employee> result = (await _employeeService.Object.ListAsync()).ToList();
+            List<Employee> result = (await _employeeService.ListAsync()).ToList();
             Assert.AreEqual(result.Count, 10);
+            Assert.AreEqual(result.First().Name, "John");
         }
 
         [TestMethod()]
-        public void GetEmployeeForShiftTest()
+        public async Task GetEmployeeForShiftNoEmployeeTest()
         {
-            Assert.Fail();
+            Employee previousShiftEmployee = null;
+            List<Employee> employeesWithTwoShifts = (await _employeeService.ListAsync()).ToList();
+            Employee chosen = await _employeeService.GetEmployeeForShift(previousShiftEmployee, employeesWithTwoShifts);
+            Assert.IsNull(chosen);
         }
+
+        [TestMethod()]
+        public async Task GetEmployeeForShiftOnlyOneEmployeeTest()
+        {
+            Employee previousShiftEmployee = null;
+            List<Employee> employeesWithTwoShifts = (await _employeeService.ListAsync()).ToList();
+            Employee shouldBePicked = employeesWithTwoShifts.First();
+            employeesWithTwoShifts.Remove(shouldBePicked);
+            Employee chosen = await _employeeService.GetEmployeeForShift(previousShiftEmployee, employeesWithTwoShifts);
+            Assert.AreEqual(shouldBePicked, chosen);
+        }
+
+        [TestMethod()]
+        public async Task GetEmployeeForShiftWithPreviousBeingAddedToTwoShiftsEmployeesTest()
+        {
+            Employee previousShiftEmployee = null;
+            List<Employee> employeesWithTwoShifts = new List<Employee>();
+            int totalEmployees = (await _employeeService.ListAsync()).ToList().Count;
+            Employee chosen = await _employeeService.GetEmployeeForShift(previousShiftEmployee, employeesWithTwoShifts);
+            while (chosen != null)
+            {
+                employeesWithTwoShifts.Add(chosen);
+                chosen = await _employeeService.GetEmployeeForShift(previousShiftEmployee, employeesWithTwoShifts);                
+            }
+            Assert.AreEqual(totalEmployees, employeesWithTwoShifts.Count);
+        }
+
     }
 }
